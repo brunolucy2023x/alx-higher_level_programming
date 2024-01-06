34fbf2d5c5ce5be29fab14629a97fc5d8b902805
brunolucy2023x <lucyb5147@gmail.com>
2024-01-06 05:09:08 -0800

    5-no_c.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 4-new_in_list.py
+# Bruno Okoth
 def new_in_list(my_list, idx, element):
     listlength = len(my_list) - 1
     if (idx < 0 or idx > listlength):
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 5-no_c.py
+# Bruno Okoth
 def no_c(my_string):
     new_string = ""
     for c in my_string:
