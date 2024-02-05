424c3f7fe8d142d704ecfc312140a7f03601c167
brunolucy2023x <lucyb5147@gmail.com>
2024-02-05 05:41:48 -0800

    Forth Update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Bruno Okoth
+# brunookoth44@gmail.com
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """method for calculated area"""
+
+        raise Exception("area() is not implemented")
