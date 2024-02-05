637dd6117041aef2218f3c447f21b75f0cb84950
brunolucy2023x <lucyb5147@gmail.com>
2024-02-05 05:36:57 -0800

    Second Update

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Bruno Okoth
+# brunookoth44@gmail.com
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
