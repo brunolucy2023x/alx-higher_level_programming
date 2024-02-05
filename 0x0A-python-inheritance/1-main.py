406a900868abcad7163870ac2e6dc18461fb2aef
brunolucy2023x <lucyb5147@gmail.com>
2024-02-05 05:10:55 -0800

    Second Update

---
+++ 0x0A-python-inheritance/1-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-my_list.py
+# Bruno Okoth
+# brunookoth44@gmail.com
+MyList = __import__('1-my_list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
