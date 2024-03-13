b3dfbbed5ae1199d6810af41840bcdf265d89a84
brunolucy2023x <lucyb5147@gmail.com>
2024-03-13 05:01:43 -0700

    Third update

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- brunookoth44@gmail.com
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
