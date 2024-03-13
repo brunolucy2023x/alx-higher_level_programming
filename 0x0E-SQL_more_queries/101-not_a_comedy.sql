3bf5e409965dbdf0fd97652922bf69d72da6f3fe
brunolucy2023x <lucyb5147@gmail.com>
2024-03-13 05:28:03 -0700

    new update

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,17 @@
- -- @brunookoth44@gmail.com
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
 
-SELECT tv_shows.title FROM tv_shows
-WHERE tv_shows.id NOT IN (
-      SELECT tv_shows.id FROM tv_shows
-      JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-      JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
-      WHERE tv_genres.name = "Comedy" )
-ORDER BY tv_shows.title;
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
